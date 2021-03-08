df211ea12e84da25fb13ca9616d0febcb2cfdbae
Jitendrakumar Sodvadiya <Mahhid444@gmail.com>
2021-03-08 11:05:10 -0600

    shows map

--- KeepCompany/KeepCompany/Views/MapView.swift
+++ KeepCompany/KeepCompany/Views/MapView.swift
@@ -4,9 +4,10 @@
 //
 //  Created by krupa on 3/3/21.
 //
-
+import MapKit
+import Firebase
 import SwiftUI
-
+/*
 struct MapView: View {
     var body: some View {
         Text("Hello, World!")
@@ -18,3 +19,23 @@ struct MapView_Previews: PreviewProvider {
         MapView()
     }
 }
+*/
+
+struct MapView: View {
+    
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 35.67, longitude: 139.65),
+        span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+    
+    var body: some View {
+        Map(coordinateRegion: $region)
+    }
+   
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
