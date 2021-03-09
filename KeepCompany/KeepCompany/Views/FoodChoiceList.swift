223036c93275369be180c4511abd1a1f5d451ab4
krupa <krupamp231@gmail.com>
2021-03-09 15:03:08 -0800

    navigation issue

--- KeepCompany/KeepCompany/Views/FoodChoiceList.swift
+++ KeepCompany/KeepCompany/Views/FoodChoiceList.swift
@@ -10,41 +10,41 @@ import SwiftUI
 struct FoodChoiceList: View {
     
     @State private var showingAlert = false
-//    @State var selection: Int? = nil
+    @State var selection: Int? = nil
     @State private var clickedItem = ""
     @ObservedObject var cuisineModel = CuisineModel()
     @State private var isUserConfirm: Bool = false
 
 
     var body: some View {
-        //NavigationView{
+       //NavigationView{
             List(cuisineModel.arrCuisine,id:\.self){ name in
                 Text(name).font(.subheadline).foregroundColor(.black)
-                NavigationLink(destination: MapView()){}
                 Button(action: {
                     userDefaults.setValue(name, forKey: TextConstant.SELECTDEDRES)
-                    //self.showingAlert = true
+                     self.showingAlert = true
                 }) {Text("")}
-            }
-//            NavigationLink(destination:MapView(), isActive: $isUserConfirm,
-//                         label: { EmptyView() })
-//            .alert(isPresented:self.$showingAlert, content: {
-//                Alert(title:Text("Confirm Dialog"), message: Text(AlertMessage.CONFIRM), primaryButton: Alert.Button.default(Text(AlertMessage.BTNCONFIRM)){
-//                    if let userId = userDefaults.object(forKey: TextConstant.USERID) as? String{
-//                        var name:String = "Test"
-//                        if let userName = userDefaults.object(forKey: TextConstant.USERNAME) as? String{
-//                            name = userName
-//                        }
-//                        firestoreInstace.collection(FirebaseCollection.userIntrest).document(userId).setData(["name":name]) { (error) in
-//                            if let err = error{
-//                                print(err)
-//                            }else{
-//                                self.isUserConfirm = true
-//                            }
-//                        }
-//                    }
-//                }, secondaryButton: .cancel())
-//           })
+            NavigationLink(destination:MapView(), isActive: $isUserConfirm,label: { EmptyView() })
+          }
+            .alert(isPresented:self.$showingAlert, content: {
+                Alert(title:Text("Confirm Dialog"), message: Text(AlertMessage.CONFIRM), primaryButton: Alert.Button.default(Text(AlertMessage.BTNCONFIRM)){
+                    self.isUserConfirm = true
+                    if let userId = userDefaults.object(forKey: TextConstant.USERID) as? String{
+                        var name:String = "Test"
+                        if let userName = userDefaults.object(forKey: TextConstant.USERNAME) as? String{
+                            name = userName
+                        }
+                        firestoreInstace.collection(FirebaseCollection.userIntrest).document(userId).setData(["name":name]) { (error) in
+                            if let err = error{
+                                print(err)
+                            }else{
+                                
+                            }
+                        }
+                    }
+                }, secondaryButton: .cancel())
+           })
+
             .navigationTitle(NavigationTitle.CATEGORIES).navigationBarBackButtonHidden(true)
             .navigationBarTitleDisplayMode(.inline)
     
