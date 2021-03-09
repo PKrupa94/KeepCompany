74165b5047b5653970ff4360c04951388729f246
krupa <krupamp231@gmail.com>
2021-03-09 15:45:58 -0800

    Navigation issue fix

--- KeepCompany/KeepCompany/Views/MapView.swift
+++ KeepCompany/KeepCompany/Views/MapView.swift
@@ -23,16 +23,42 @@ struct MapView_Previews: PreviewProvider {
 
 struct MapView: View {
     
-    @ObservedObject var arrCoodinates = locationOnMap()
+    //@ObservedObject var arrCoodinates = locationOnMap()
+   @State var arrRestaurantCoordinates:[MyAnnotationItem] = []
+   let selectedOption:String
     
     @State private var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude: 34.237923, longitude: -118.530197),
         span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
     
     var body: some View {
-        Map(coordinateRegion: $region, annotationItems: arrCoodinates.arrRestaurantCoordinates) { restaurant in
-            MapMarker(coordinate: restaurant.coordinates)}
+        VStack {
+            Map(coordinateRegion: $region, annotationItems: arrRestaurantCoordinates) { restaurant in
+                        MapMarker(coordinate: restaurant.coordinates)}
+        }.onAppear {
+            getCoordinates()
+        }
+    
     }
+    
+    func getCoordinates(){
+      //  if let getRest = userDefaults.object(forKey: TextConstant.SELECTDEDRES) as? String{
+            let docRef = firestoreInstace.collection(FirebaseCollection.CuisineList).document(selectedOption)
+                    docRef.collection(selectedOption).getDocuments() {(snapshot, error) in
+                        if let error = error{
+                            print("Error getting Documents: \(error)")
+                        }else{
+                            for doc in snapshot!.documents{
+                                if let coords = doc.get(FirebaseCollection.Coordinates){
+                                    let point = coords as! GeoPoint
+                                    let coodinates = MyAnnotationItem(coordinates: .init(latitude: point.latitude, longitude: point.longitude))
+                                    self.arrRestaurantCoordinates.append(coodinates)
+                                }
+                            }
+                            print(self.arrRestaurantCoordinates)
+                        }
+                    }
+        }
     
 }
 
@@ -41,5 +67,4 @@ struct MapView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
 
