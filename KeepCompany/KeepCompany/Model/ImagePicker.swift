cd60149b60430425a99e97a42a55ba4d0d091263
Robin Malhotra <robinmalhotra1396@gmail.com>
2021-04-30 06:22:53 +0000

    ImagePicker

--- KeepCompany/KeepCompany/Model/ImagePicker.swift
+++ KeepCompany/KeepCompany/Model/ImagePicker.swift
@@ -1,61 +1,79 @@
-//
-//  ImagePicker.swift
-//  OTP
-//
-//  Created by Kavsoft on 18/01/20.
-//  Copyright © 2020 Kavsoft. All rights reserved.
-//
 
 import SwiftUI
+import Firebase
+import Combine
 
-struct ImagePicker : UIViewControllerRepresentable {
+
+class ImagePicker: ObservableObject {
+    static let shared: ImagePicker = ImagePicker()
+    private init() {}
     
-    @Binding var picker : Bool
-    @Binding var imagedata : Data
+    let view = ImagePicker.View()
+    let coordinator = ImagePicker.Coordinator()
     
-    func makeCoordinator() -> ImagePicker.Coordinator {
+    let willChange = PassthroughSubject<Image?, Never>()
+    @Published var image:Image? = nil {
         
-        return ImagePicker.Coordinator(parent1: self)
-    }
-    
-    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
-        
-        let picker = UIImagePickerController()
-        picker.sourceType = .photoLibrary
-        picker.delegate = context.coordinator
-        return picker
-    }
-    
-    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
-        
-        
-    }
-    
-    class Coordinator : NSObject,UIImagePickerControllerDelegate,UINavigationControllerDelegate{
-        
-        var parent : ImagePicker
-        
-        init(parent1 : ImagePicker) {
-            
-            parent = parent1
+        didSet{ if image != nil {
+            willChange.send(image)
         }
+        }
+    }
+    
+}
+
+extension ImagePicker {
+    
+    class Coordinator: NSObject, UINavigationControllerDelegate,
+                       UIImagePickerControllerDelegate {
         
         func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-            
-            self.parent.picker.toggle()
+            picker.dismiss(animated: true)
         }
         
-        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        func imagePickerController(_ picker: UIImagePickerController,
+                                   didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any])
+        {
+            let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+            
+            ImagePicker.shared.image = Image(uiImage: image)
+            picker.dismiss(animated: true)
             
             
-            let image = info[.originalImage] as! UIImage
+        }
+        
+    }
+}
+
+
+
+extension ImagePicker {
+    struct View: UIViewControllerRepresentable {
+        
+        func makeCoordinator() -> Coordinator {
+            return ImagePicker.shared.coordinator
+        }
+        
+        
+        
+        func makeUIViewController(context:
+                                    UIViewControllerRepresentableContext<ImagePicker.View>) ->
+        UIImagePickerController {
+            let imagePickerController = UIImagePickerController()
+            imagePickerController.delegate = context.coordinator
+            return imagePickerController
+        }
+        
+        func updateUIViewController(_ uiViewController: UIImagePickerController,
+                                    context: UIViewControllerRepresentableContext<ImagePicker.View>) {
             
-            let data = image.jpegData(compressionQuality: 0.45)
-            
-            self.parent.imagedata = data!
-            
-            self.parent.picker.toggle()
         }
     }
 }
+
+
+
+
+
+
 
